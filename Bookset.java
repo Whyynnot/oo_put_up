35a606336e11926313d89baf6dabd4b41ecf3c4d
Li Haoran <1620146606@qq.com>
2021-03-02 15:06:22 +0800

    add Bookset.java

---
+++ Bookset.java
@@ -0,0 +1,29 @@
+public class Bookset {
+    private String name;
+    private double price;
+    private int num;
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public double getPrice() {
+        return price;
+    }
+    
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    
+    public int getNum() {
+        return num;
+    }
+    
+    public void setNum(int num) {
+        this.num = num;
+    }
+}
